5acb9ef0ef50d427bb18abda4ba5fac106f16710
jeongHooon <pkekzm94@naver.com>
2017-06-10 22:47:55 +0900

    Add files via upload

--- GUI.py
+++ GUI.py
@@ -32,6 +32,16 @@ def FinancialTermMeaning():
     searchList1.place(x=110, y=50)
     searchList2 = Listbox(window, width=20)
     searchList2.place(x=420, y=50)
+def StockInfo():
+    global check
+    global searchList1
+    global searchList2
+    searchList1.destroy()
+    searchList2.destroy()
+    searchList1 = Listbox(window, width=30)
+    searchList1.place(x=110, y=50)
+    searchList2 = Listbox(window, width=20)
+    searchList2.place(x=420, y=50)
 def start():
     global  check
     global searchList1
@@ -44,7 +54,7 @@ def start():
     button1.place(x=10,y=10)
     button2 = Button(window, text="기업정보\n조회", font = 20,cursor="hand2", command = IssucoBasicInfo)
     button2.place(x=10,y=70)
-    button3 = Button(window, text="주식정보\n조회", font = 20,cursor="hand2")
+    button3 = Button(window, text="주식정보\n조회", font = 20,cursor="hand2", command = StockInfo)
     button3.place(x=10,y=130)
     button4 = Button(window, text="금융용어\n조회", font = 20,cursor="hand2", command = FinancialTermMeaning)
     button4.place(x=10,y=190)
